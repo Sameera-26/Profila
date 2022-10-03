5ff942ee4c3700eefbb48d916ded2121fb8bd487
Hamza zafar <48332435+hamzaali4943@users.noreply.github.com>
2022-10-03 16:51:54 -0700

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="pt-10 md:p-8 p-4">
     <h3 class="text-center text-gray-200 font-puffinSemibold text-xl"
-      v-bind:class="{ hidden: openTab !== 1, block: openTab === 1 }">Log in to your Profila</h3>
+      :class="{ hidden: openTab !== 1, block: openTab === 1 }">Log in to your Profila</h3>
     <h3 class="text-center text-gray-200 font-puffinSemibold text-xl"
-      v-bind:class="{ hidden: openTab !== 2, block: openTab === 2 }">Sign up for free</h3>
+      :class="{ hidden: openTab !== 2, block: openTab === 2 }">Sign up for free</h3>
     <ul class="flex rounded-lg border p-1 items-center border-gray-300 mt-8 space-x-1">
       <li class="text-sm font-bold capitalize text-center cursor-pointer w-full p-3 rounded-md"
-        v-on:click="toggleTabs(1)" v-bind:class="{
+        @click="toggleTabs(1)" :class="{
           'text-gray-900 bg-white': openTab !== 1,
           'text-gray-400 bg-gray-50': openTab === 1,
         }">
         Phone Number
       </li>
       <li class="text-sm font-bold capitalize text-center cursor-pointer w-full p-3 rounded-md"
-        v-on:click="toggleTabs(2)" v-bind:class="{
+        @click="toggleTabs(2)" v-bind:class="{
           'text-gray-900 bg-white': openTab !== 2,
           'text-gray-400 bg-gray-50': openTab === 2,
         }">
         Email
       </li>
     </ul>
-    <div v-bind:class="{ hidden: openTab !== 1, block: openTab === 1 }">
+
+    <form @submit.prevent="signin" :class="{ hidden: openTab !== 1, block: openTab === 1 }">
       <div class="grid grid-cols-6 gap-4 py-8">
         <div class="col-span-2">
           <label class="block tracking-wide text-gray-100 text-xs font-bold mb-2" for="grid-state">
@@ -51,7 +52,7 @@
         </div>
       </div>
       <div class="space-y-5">
-        <button @click="onButtonPhone" class="bg-primary-100 w-full text-white text-xs py-3.5 rounded-lg">
+        <button type="submit" class="bg-primary-100 w-full text-white text-xs py-3.5 rounded-lg">
           Log in with code
         </button>
         <div class="flex items-center justify-center text-sm tracking-tight">
@@ -59,8 +60,9 @@
           <a href="/" class="text-primary-500 px-1 font-semibold">Sign up</a>
         </div>
       </div>
-    </div>
-    <div v-bind:class="{ hidden: openTab !== 2, block: openTab === 2 }">
+    </form>
+
+    <form @submit.prevent="signup" :class="{ hidden: openTab !== 2, block: openTab === 2 }">
       <p class="text-center text-sm pt-7 text-gray-400">
         You will receive an email with a link to confirm registration.
       </p>
@@ -71,10 +73,9 @@
         <input
           class="appearance-none block w-full text-xs bg-gray-150 text-gray-800 border border-gray-600 rounded-lg p-4 leading-tight focus:outline-none placeholder-gray-800"
           type="text" v-model="state.email" placeholder="Your Email" />
-        <span v-if="v$.email.$error"> {{ v$.email.$errors[0].$message }} </span>
       </div>
       <div class="space-y-5">
-        <button @click="onButtonPhone" class="bg-primary-100 w-full text-white text-xs py-3.5 rounded-lg">
+        <button type="submit" class="bg-primary-100 w-full text-white text-xs py-3.5 rounded-lg">
           Get Started
         </button>
         <div class="flex items-center justify-center text-sm tracking-tight">
@@ -82,68 +83,47 @@
           <a href="/" class="text-primary-500 px-1 font-semibold">Sign in</a>
         </div>
       </div>
-    </div>
+    </form>
   </div>
 
 </template>
 
-<script>
-import useValidate, { useVuelidate } from '@vuelidate/core'
+<script setup>
+import useValidate from '@vuelidate/core'
 import { required, email } from '@vuelidate/validators'
-import { reactive, computed } from 'vue';
-export default {
-  name: "Login",
-  setup() {
-    const state = reactive({
-      email: '',
-      number: ''
-    })
-    const rules = computed(() => {
-      return {
-        email: { required, email },
+import { reactive, computed, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
-      }
-    })
-    const v$ = useVuelidate(rules, state)
-    return {
-      state,
-      v$
+  const state = reactive({
+    email: '',
+    number: ''
+  })
+
+  const rules = computed(() => {
+    email: { required, email  }
+  })
+
+  const v$ = useValidate(rules, state)
+
+  const openTab = ref(1)
+
+  const toggleTabs = (tabNumber) => {
+    openTab.value = tabNumber;
+  }
+
+  const onButtonPhone = () => {
+    router.push('/phoneLink')
+  }
+
+  const router = useRouter()
+
+  const signup = () => {
+    v$.$validate() // checks all inputs
+    if (!v$.$error) {
+      router.push('/emailLink')
     }
-  },
-
-  data() {
-    return {
-      openTab: 1,
-      v$: useVuelidate(),
-      email: '',
-
-    };
-  },
-  methods: {
-    toggleTabs: function (tabNumber) {
-      this.openTab = tabNumber;
-    },
-    onButtonPhone() {
-
-      this.$router.push('/phoneLink')
-
-
-    },
-    onButtonEmail() {
-      console.log(this.v$)
-
-
-
-      this.v$.$validate() // checks all inputs
-      if (!this.v$.$error) {
-        this.$router.push('/emailLink')
-      }
-    },
-  },
-  validations() {
-    return {
-      email: { required, email },
-    }
-  },
-}
+  }
+  const signin = () => {
+    router.push('/emailLink')
+  }
 </script>
